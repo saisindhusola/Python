255c07eb79bea17e16be4d748a4cf78d621c8e3c
Sola Sai Sindhu <saisindhusrinivas@gmail.com>
2023-08-16 22:07:57 -0700

    variables examples

--- print_example.py
+++ print_example.py
@@ -1,1 +1,3 @@
 print("This is an example statement.")
+print("20 days are " +  str(20*24*60)  + " minutes.")
+print(f"20 days are {20*24*60} minutes.")
---
+++ variables.py
@@ -0,0 +1,14 @@
+print(f"20 days are {20*24*60*60} seconds")
+
+caluclation_to_units = 24*60*60
+name_of_unit = "seconds"
+print(f"20 days are {20 * caluclation_to_units} {name_of_unit}")
+print(f"30 days are {30 * caluclation_to_units} {name_of_unit}")
+print(f"60 days are {60 * caluclation_to_units} {name_of_unit}")
+
+print(f"20 days are {20*24*60} minutes")
+caluclation_to_units_minutes = 24*60
+name_of_unit_minutes = "minutes"
+print(f"20 days are {20 * caluclation_to_units_minutes} {name_of_unit_minutes}")
+print(f"30 days are {30 * caluclation_to_units_minutes} {name_of_unit_minutes}")
+print(f"60 days are {60 * caluclation_to_units_minutes} {name_of_unit_minutes}")
