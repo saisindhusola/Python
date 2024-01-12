cdb41b4bc620c5a90eaa2988b75e5be5d8788b13
Sola Sai Sindhu <saisindhusrinivas@gmail.com>
2024-01-12 14:37:13 -0800

    calculator function

---
+++ advance_calc.py
@@ -0,0 +1,16 @@
+#This program contains multiple functions which can be used as module.git
+numOne = 14
+numTwo = 7
+
+def addition():
+    add = (numOne + numTwo)
+    print(add)
+def subtraction():
+    sub = (numOne - numTwo)
+    print(sub)
+def multiplication():
+    mul = (numOne * numTwo)
+    print(mul)
+addition()
+subtraction()
+multiplication()
