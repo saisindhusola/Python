14db3c2c403d34cdf73cc57d2c3cc26d61b81acc
Sola Sai Sindhu <saisindhusrinivas@gmail.com>
2024-01-12 15:11:58 -0800

    Module example

--- moduleEx.py
+++ moduleEx.py
@@ -1,4 +1,6 @@
 #Here file(advanc_calc) containing multiple functions is used as a module.
-import advance_calc as basic_calc
+import advance_calc as basic_calc #To use all functions in the module.
+from advance_calc import addition #To use particular function in the module.
 
 basic_calc
+addition()
