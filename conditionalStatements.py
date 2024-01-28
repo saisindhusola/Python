0acf26a9193232464b764a46d9adf783c1eb551b
Sola Sai Sindhu <saisindhusrinivas@gmail.com>
2024-01-28 11:51:20 -0800

    Conditional Staements Examples

---
+++ conditionalStatements.py
@@ -0,0 +1,20 @@
+# If Statemet
+x = 6
+if x > 3:
+    print("x is greater than 3")
+
+#elif(else-if) statement
+y = 8
+if y > 10:
+    print("y is greater than 10")
+elif y > 5:
+    print("y is greater than 5 but not 10")
+else:
+    print("y is not greater than 8")
+
+#else statement
+z = 3
+if z > 5:
+    print("z is greater than 5")
+else:
+    print("z is less than 5")
