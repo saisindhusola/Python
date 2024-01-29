c0e87eebb668a6c4e2f9db710f694ab91a15f517
Sola Sai Sindhu <saisindhusrinivas@gmail.com>
2024-01-29 12:49:34 -0800

    Tuple examples

---
+++ tuple_example.py
@@ -0,0 +1,19 @@
+# Same as List but Tuple are mutable(can't change the values)
+tup = (21, 36, 29)
+print(tup)
+
+print(tup[1]) # gives the element with index 1
+
+my_tuple = ('Apple', 'Banana', 'Orange', 'Orange')
+print(my_tuple)
+print(len(my_tuple))
+
+#creating tuple with one item
+flower_tuple_one =('Rose') #Requires Coma for single item in a tuple
+print(type(flower_tuple_one))
+flower_tuple_two =('Rose',)
+print(type(flower_tuple_two))
+
+#A tuple with all data types
+all_data_types = ("Rose", True, 0.3, 22, "Banana")
+print(all_data_types)
