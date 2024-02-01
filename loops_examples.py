c5d6c4ea06760b82b9e0cb08eb8ac8b6ec9e112c
Sola Sai Sindhu <saisindhusrinivas@gmail.com>
2024-02-01 14:57:43 -0800

    For and While loops

---
+++ loops_examples.py
@@ -0,0 +1,17 @@
+#While loop
+#syntax: while condition:
+count = 0
+while count < 5:
+        print(count)
+        count += 1
+
+        
+# For Loop
+# Syntax: for variable in sequence:
+#Example
+fruits = ["Apple","Banana","Orange","Cherry"]
+for fruit in fruits:
+    print(fruit)
+    
+
+
