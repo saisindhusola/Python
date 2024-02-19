7d618dae7a44980ea0e2cae26392ab920d6d07ef
Sola Sai Sindhu <saisindhusrinivas@gmail.com>
2024-02-19 15:15:09 -0800

    Dictioneries Example

--- dictioneries.py
+++ dictioneries.py
@@ -4,3 +4,24 @@ student_details = {
     'class': '5'
 }
 print(student_details['name']) #Output prints as Sai.
+
+
+student_details_list = [
+    {
+        'name':'Sidhu',
+        'age':11,
+        'class':'5'
+    },
+    {
+        'name':'Shan',
+        'Age':11,
+        'class':'6'
+    },
+    {
+        'name':'Ram',
+        'age':10,
+        'class':'5'
+    }
+
+]
+print(student_details_list[1]) # prints the output of details of 1st index.
